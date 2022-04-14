e662203479d10f2481f57bc74a52f20a2ae2db7d
jecky <jecky@example.com>
2022-04-15 00:29:18 +0800

    u

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
-fn main() {
-  // TODO: 6.3. if let 简洁控制流
-  /*
-   * 3u8 3 是 u8
-   */
-  let config_max = Some(3u8); 
-  match config_max {
-    Some(max) => println!("The maximum is configured to be {}", max),
-    _ => (),
-  }
-  // The maximum is configured to be 3
+use aggregator::{Summar, Tweet}; // 使用库
 
-  if let Some(max) = config_max {
-    println!("The maximum is configured to be {}", max);
-  }
-  // The maximum is configured to be 3
-  println!("3u8: {}", 8u8);  // 3u8: 8
- 
-  
-  let config_max = Some(6u8); 
-  if let Some(max) = config_max {
-    println!("The maximum is configured to be {}", max);
-  }
-  
+// trait：定义共享的行为
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
